8b642cb170f6604a9e069c563b7f2aeb2d7d6667
chitkulasaikiran <chitkulasaikiran@gmail.com>
2022-11-09 10:51:53 +0530

    sai

---
+++ ZippyFrontEnd/src/app/components/restaurant-info/restaurant-info.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { ShareInformationService } from '../../service/share-information.service';
+import { Router } from '@angular/router';
+import { NgToastService } from 'ng-angular-popup';
+import { FavouriteService } from './../../service/favourite.service';
+import { FavouriteCuisine } from './../../model/favourite-cuisine';
+import { RestaurantComponent } from '../restaurant/restaurant.component';
+import { RestaurantService } from 'src/app/service/restaurant.service';
+
+@Component({
+  selector: 'app-restaurant-info',
+  templateUrl: './restaurant-info.component.html',
+  styleUrls: ['./restaurant-info.component.css']
+})
+
+export class RestaurantInfoComponent implements OnInit {
+   restauratantInfo : any ;
+   cuisinesInfo:any;
+
+  constructor(private restaurantService:RestaurantService,
+    private favouriteService : FavouriteService ,
+    private shareRestautant : ShareInformationService ,
+    private router : Router,
+    private toast : NgToastService) { }
+
+  ngOnInit(): void {
+   this.restauratantInfo = this.shareRestautant.getData();
+   console.log("restaurant info")
+   this.cuisinesInfo=this.restaurantService.getAllCuisines();
+   console.log(this.restauratantInfo);
+  //  if(this.restauratantInfo == undefined)
+  //    {
+  //     this.restauratantInfo = '';
+  //     this.router.navigate(['/restaurant']);
+  //    }
+  }
+
+  public favourites: FavouriteCuisine[];
+  addFavCuisine(data: any) {
+    const id = Math.floor((Math.random() * 10000) + 1);
+    this.favourites = [
+      new FavouriteCuisine(
+        data.cuisineId = id,
+        data.cuisineName,
+        data.cuisineImage,
+        window.localStorage.getItem('userEmail'),
+        data.price)]
+    console.log(this.favourites);
+    this.favouriteService.addCuisinesToCart(this.favourites[0]);
+  }
+}
---
+++ ZippyFrontEnd/src/app/service/restaurant.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class RestaurantService {
+ 
+        id:any;
+  constructor( private http : HttpClient) { }
+
+
+
+  getAllRestaurants() {
+    const restauranturl = 'http://localhost:9000/foodieApp/restaurant/getRestaurant';
+    return this.http.get<any>(restauranturl);
+  }
+
+  getRestaurantByLocation(location : string) {
+    const locationUrl = 'http://localhost:9000/foodieApp/restaurant/restaurant-by-location/';
+   return this.http.get<any>(locationUrl+location);
+  }
+
+  getAllCuisines() {
+    const cuisineUrl = 'http://localhost:9000/foodieApp/cuisine/getAllCuisine';
+    return this.http.get<any>(cuisineUrl);
+  }
+
+  getAllCuisineOfARestaurant(id:any){
+    const cuisineRestUrl='http://localhost:9000/foodieApp/restaurant/getCuisines/'+id;
+      return this.http.get<any>(cuisineRestUrl);
+  }
+}
+
